cbbdfa26fdb04f976237145e29a1309df15807ab
Michael Li <alimy@gility.net>
2018-12-20 02:27:38 +0800

    change provider as a logus's submodule

--- provider/gin_logger.go
+++ provider/gin_logger.go
@@ -1,11 +1,15 @@
 package provider
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/unisx/logus"
+)
 
 // GinLogger instances a Logger middleware that use logus write the logs to zap
 func GinLogger() gin.HandlerFunc {
 	return gin.HandlerFunc(func (context *gin.Context) {
 		// TODO
+		logus.Info("gin handle", logus.String("clientIP", context.ClientIP()))
 	})
 }
 
@@ -18,5 +22,6 @@ func GinErrorLogger() gin.HandlerFunc {
 func GinErrorLoggerT(typ gin.ErrorType) gin.HandlerFunc {
 	return gin.HandlerFunc(func (context *gin.Context) {
 		// TODO
+		logus.Info("gin handle", logus.String("clientIP", context.ClientIP()))
 	})
 }
---
+++ provider/go.mod
@@ -0,0 +1,14 @@
+module github.com/unisx/logus/provider
+
+require (
+	github.com/gin-gonic/gin v1.3.0
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/pkg/errors v0.8.0
+	github.com/stretchr/testify v1.2.2
+	github.com/ugorji/go/codec v0.0.0-20181209151446-772ced7fd4c2 // indirect
+	github.com/unisx/logus v0.2.1
+	go.uber.org/zap v1.9.1
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
